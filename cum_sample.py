910501b9a624faaca11d183e3305608848f87a8e
wafeiwang <wafeiwang@tencent.com>
2024-06-06 19:48:32 +0800

    add torch functions

--- cum_sample.py
+++ cum_sample.py
@@ -9,17 +9,67 @@ import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch
 
+
 class CumSample(nn.Module):
     def __init__(self, alpha_size) -> None:
         super().__init__()
         self.alpha = nn.Parameter(torch.randn(alpha_size))
-    
+
     def forward(self, data_dir):
-        y = cum_equations(self.alpha, data_dir)
-    
+        y = torch_cum_equations(self.alpha, data_dir)
+
         return y
 
-def cum_equation(alpha: np.ndarray, daily_sample: np.ndarray, cum_sample: np.ndarray) -> np.ndarray:
+
+def torch_cum_equation(
+    alpha: torch.tensor, daily_sample: torch.tensor, cum_sample: torch.tensor
+) -> torch.tensor:
+    t = alpha.shape[0] + 1
+    equ = torch.zeros(t - 1)
+    aug_alpha = torch.zeros(t)
+    aug_alpha[0] = 1
+    aug_alpha[1:] = alpha
+
+    b = torch.zeros(t)
+    b[0] = daily_sample[0]
+    for i in range(1, min(t, daily_sample.shape[0])):
+        #b[i] = daily_sample[i] - torch.sum(b[:i] * torch.flip(aug_alpha[1 : (i + 1)], dims=[0]))
+        equ[i - 1] = cum_sample[i] - b[: (i + 1)].sum()
+
+    return equ
+
+
+def torch_cum_equations(alpha: torch.tensor, data_dir: str):
+    t = alpha.shape[0]
+    equ = torch.zeros(t)
+    aug_alpha = torch.zeros(t + 1)
+    aug_alpha[0] = 1
+    aug_alpha[1:] = alpha
+
+    k = torch.zeros(t)
+    files = os.listdir(data_dir)
+
+    for file in files:
+        file_path = os.path.join(data_dir, file)
+        if os.path.isfile(file_path):
+            # Use numpy to read the CSV file
+            df = np.genfromtxt(file_path, delimiter=",", skip_header=1)
+            # Convert the numpy array to a PyTorch tensor
+            df = torch.from_numpy(df)
+            n_values = df[:, 0]
+            cum_values = df[:, 1]
+            equ_f = torch_cum_equation(alpha, n_values, cum_values)
+            #k += 1 - (equ_f == 0).type(torch.int)
+            equ += equ_f
+
+    #equ = equ / k
+    new_equ = equ.clone()
+    return torch.sum(new_equ * new_equ)
+
+
+def cum_equation(
+    alpha: np.ndarray, daily_sample: np.ndarray, cum_sample: np.ndarray
+) -> np.ndarray:
     t = alpha.shape[0] + 1
     equ = np.zeros(t)
     aug_alpha = np.zeros(t)
@@ -115,3 +165,22 @@ if __name__ == "__main__":
 
     plt.tight_layout()
     plt.show()
+
+    model = CumSample(10)
+    loss = model(args.data_dir)
+    #optimizer = torch.optim.LBFGS([model.alpha])
+    optimizer = torch.optim.Adam(model.parameters())
+
+
+    def closure():
+        optimizer.zero_grad()
+        loss = model(args.data_dir)
+        loss.backward()
+        return loss
+    num_epochs = 100
+    #optimizer.zero_grad()
+    for epoch in range(num_epochs):
+        with torch.autograd.set_detect_anomaly(True):
+            optimizer.step(closure)
+            loss = model(args.data_dir)
+            print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {loss}") 
